b34563e152a1c085e1fefa702337e76220ca3c93
null1923 <129907487+null1923@users.noreply.github.com>
2023-11-30 01:36:15 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-quickstart';
 }
+// app.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Buraya yukarıdaki örnek kodu ekle
+}
